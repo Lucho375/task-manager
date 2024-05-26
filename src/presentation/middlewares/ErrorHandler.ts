fabb1c6842ca259ca17cced7f28243095b36bcc8
Luciano Rodríguez <luciano.rodriguez59@gmail.com>
2024-05-26 14:51:08 -0300

    add logger to errorhandler

--- src/presentation/middlewares/ErrorHandler.ts
+++ src/presentation/middlewares/ErrorHandler.ts
@@ -2,12 +2,9 @@ import { type NextFunction, type Request, type Response } from 'express'
 import jwt from 'jsonwebtoken'
 import { ZodError } from 'zod'
 import { CustomError } from '../../domain/index.js'
+import { errorLogger } from '../../infrastructure/index.js'
 
 export const ErrorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
-  console.log({
-    error,
-  })
-
   if (error instanceof CustomError) {
     return res.status(error.statusCode).send({ message: error.message })
   }
@@ -18,5 +15,12 @@ export const ErrorHandler = (error: Error, req: Request, res: Response, next: Ne
 
   if (error instanceof ZodError) return res.status(400).send(error.issues.map((issue) => ({ [issue.path[0]]: issue.message })))
 
+  errorLogger.error({
+    message: error.message,
+    stack: error.stack,
+    name: error.name,
+    clientIP: req.ip,
+    userAgent: req.headers['user-agent'],
+  })
   return res.status(500).send('Internal Server Error')
 }
