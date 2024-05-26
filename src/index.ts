8b24170113c7261aa78b3fd15b76abb38b5f5c42
Luciano Rodríguez <luciano.rodriguez59@gmail.com>
2024-05-26 14:51:24 -0300

    add winston

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { appConfig } from './config/AppConfig.js'
-import { DatabaseFactory, DatabaseType } from './infrastructure/index.js'
+import { AppConfig } from './config/AppConfig.js'
+import { DatabaseFactory, DatabaseType, errorLogger } from './infrastructure/index.js'
 import { AppExpress } from './presentation/application/AppExpress.js'
 ;(async () => {
   try {
-    const db = DatabaseFactory.create(appConfig.DB_TYPE as DatabaseType)
-    await db.connect(appConfig.DATABASE_URI)
-    const app = new AppExpress(appConfig.PORT)
+    const db = DatabaseFactory.create(AppConfig.getInstance().DB_TYPE as DatabaseType)
+    await db.connect(AppConfig.getInstance().DB_URI)
+    const app = new AppExpress(AppConfig.getInstance().PORT)
     app.listen()
-  } catch (error) {
-    console.log(error)
+  } catch (error: any) {
+    errorLogger.error(error)
   }
 })()
